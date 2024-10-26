c22dba196f318f2b2740407b49845fc521ff7403
rafaelurben <github@rafaelurben.ch>
2024-10-26 11:54:16 +0200

    feat(i18n): Started translating

--- account/conf.py
+++ account/conf.py
@@ -1,22 +1,29 @@
 from django.conf import settings
+from django.utils.translation import gettext_lazy as _
 
-BACKENDS_DISPLAY_CONFIG = {
-    'google-oauth2': {
-        'title': 'Google',
-        'icon': 'account/icons/logo_google-oauth2.svg',
-    },
-    'github': {
-        'title': 'GitHub',
-        'icon': 'account/icons/logo_github.svg',
-    },
-    'discord': {
-        'title': 'Discord',
-        'icon': 'account/icons/logo_discord.svg',
-    },
-    'azuread-oauth2': {
-        'title': 'Microsoft',
-        'icon': 'account/icons/logo_azuread-oauth2.svg',
-    },
-    **getattr(settings, 'ACCOUNT_BACKENDS_DISPLAY_CONFIG', dict()),
-}
 
+class Config:
+    APPLICATION_HEADER = getattr(settings, 'ACCOUNT_APPLICATION_HEADER', _('Account management'))
+    APPLICATION_NAME = getattr(settings, 'ACCOUNT_APPLICATION_NAME', _('this webapp'))
+
+    BACKENDS_DISPLAY_CONFIG = {
+        'google-oauth2': {
+            'title': 'Google',
+            'icon': 'account/icons/logo_google-oauth2.svg',
+        },
+        'github': {
+            'title': 'GitHub',
+            'icon': 'account/icons/logo_github.svg',
+        },
+        'discord': {
+            'title': 'Discord',
+            'icon': 'account/icons/logo_discord.svg',
+        },
+        'azuread-oauth2': {
+            'title': 'Microsoft',
+            'icon': 'account/icons/logo_azuread-oauth2.svg',
+        },
+        **getattr(settings, 'ACCOUNT_BACKENDS_DISPLAY_CONFIG', dict()),
+    }
+
+    HELP_SUPPORT_OPTIONS: list[tuple[str]] = getattr(settings, 'ACCOUNT_HELP_SUPPORT_OPTIONS', None)
--- account/templatetags/account_tags.py
+++ account/templatetags/account_tags.py
@@ -1,9 +1,14 @@
 from django import template
-import account.conf as conf
+from account.conf import Config
 
 register = template.Library()
 
 
 @register.simple_tag(name="backend_display_config")
 def get_backend_display_config(backend):
-    return conf.BACKENDS_DISPLAY_CONFIG.get(backend, dict())
+    return Config.BACKENDS_DISPLAY_CONFIG.get(backend, dict())
+
+
+@register.simple_tag(name="account_config")
+def get_account_config(option):
+    return getattr(Config, option, None)
