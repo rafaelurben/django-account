aa1a526b60a605d67094ecfb1107f3ec9232425a
rafaelurben <github@rafaelurben.ch>
2025-07-09 10:46:20 +0200

    chore: Update config format to support better config validation

--- account/templatetags/account_tags.py
+++ account/templatetags/account_tags.py
@@ -1,14 +1,14 @@
 from django import template
-from account.conf import Config
+from account.conf import config
 
 register = template.Library()
 
 
 @register.simple_tag(name="backend_display_config")
 def get_backend_display_config(backend):
-    return Config.BACKENDS_DISPLAY_CONFIG.get(backend, dict())
+    return config.BACKENDS_DISPLAY_CONFIG.get(backend, dict())
 
 
 @register.simple_tag(name="account_config")
 def get_account_config(option):
-    return getattr(Config, option, None)
+    return getattr(config, option, None)
