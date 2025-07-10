4a7cce9fffed4f5feb72527f8874a5a28590dee2
rafaelurben <github@rafaelurben.ch>
2025-07-10 19:33:02 +0200

    fix: Only catch relevant exceptions and add logging to login_with_any_backend

--- account/multidomain/altdomain/utils.py
+++ account/multidomain/altdomain/utils.py
@@ -1,7 +1,12 @@
+import logging
+
 from django.contrib.auth import login, get_backends
+from django.core.exceptions import ImproperlyConfigured
 from django.utils.translation import gettext as _
 
 from account.multidomain.exceptions import AuthFlowException
+
+logger = logging.getLogger(__name__)
 
 
 def login_with_any_backend(request, user):
@@ -11,8 +16,11 @@ def login_with_any_backend(request, user):
         if hasattr(backend, 'get_user'):
             try:
                 if backend.get_user(user.pk) == user:
-                    login(request, user, backend=backend.__module__ + '.' + backend.__class__.__name__)
+                    backend_name = backend.__module__ + '.' + backend.__class__.__name__
+                    login(request, user, backend=backend_name)
+                    logger.info("User %s logged in using backend: %s", user, backend_name)
                     return
-            except Exception:
+            except (user.__class__.DoesNotExist, AttributeError, TypeError, ValueError, ImproperlyConfigured):
+                logger.warning("Backend %s failed to authenticate user %s: %s", backend, user, exc_info=True)
                 continue
     raise AuthFlowException(_("No compatible authentication backend found for signing in. Please contact support."))
